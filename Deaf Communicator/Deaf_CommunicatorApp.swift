045cb019183f5fec88610ad25b5f8578e68a4e31
Alexander Demerjian <ademer101@gmail.com>
2023-04-17 15:24:27 -0400

    Initial Commit

---
+++ Deaf Communicator/Deaf_CommunicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deaf_CommunicatorApp.swift
+//  Deaf Communicator
+//
+//  Created by Alex Demerjian on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Deaf_CommunicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
