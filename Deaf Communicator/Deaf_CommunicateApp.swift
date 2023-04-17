11980b94344f5655a281fd89b2f331a04f12524e
Alexander Demerjian <ademer101@gmail.com>
2023-04-17 15:53:03 -0400

    Updating project structure and importing code.

--- Deaf Communicator/Deaf_CommunicateApp.swift
+++ Deaf Communicator/Deaf_CommunicateApp.swift
@@ -2,16 +2,20 @@
 //  Deaf_CommunicatorApp.swift
 //  Deaf Communicator
 //
-//  Created by Alex Demerjian on 4/17/23.
+//  Created by Alex Demerjian on 6/22/22.
 //
 
 import SwiftUI
 
 @main
 struct Deaf_CommunicatorApp: App {
+
     var body: some Scene {
-        WindowGroup {
+        
+        WindowGroup
+        {
             ContentView()
         }
     }
+    
 }
